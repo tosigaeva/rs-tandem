6f315a0cea0367b81277fcba7f4a51d6d06dcd14
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2026-02-19 17:22:10 +0100

    chore: clean ESLint unicorn config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -58,22 +58,6 @@ const eslintConfig = defineConfig([
       'unicorn/no-array-callback-reference': 'off',
       'unicorn/no-array-for-each': 'off',
       'unicorn/no-array-reduce': 'off',
-      'unicorn/no-null': 'off',
-      'unicorn/number-literal-case': 'off',
-      'unicorn/numeric-separators-style': 'off',
-      'unicorn/prevent-abbreviations': [
-        'error',
-        {
-          allowList: {
-            acc: true,
-            env: true,
-            i: true,
-            j: true,
-            props: true,
-            Props: true,
-          },
-        },
-      ],
 
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
