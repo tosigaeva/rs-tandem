09f13d4295e4059eb23c27ba0350d44589258423
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2026-02-21 19:46:30 +0100

    feat: add basic landing with mobile version

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,42 @@
+import Image from 'next/image';
+
 import { Header } from '@/components/header';
+import { PrimaryButton } from '@/components/primary-button';
 
 export default function Page() {
   return (
-    <div className="bg-background text-foreground min-h-screen">
+    <div className="bg-background text-foreground flex min-h-screen flex-col">
       <Header />
-      <main className="mx-auto flex min-h-[calc(100vh-4rem)] w-full max-w-6xl items-center justify-center px-6">
-        <h1 className="text-4xl font-bold sm:text-5xl">RS Tandem</h1>
+      <main className="mx-auto flex min-h-[calc(100vh-5rem)] w-screen items-center justify-center px-6 text-center">
+        <div>
+          <h1 className="text-5xl font-bold sm:text-6xl">RS Tandem</h1>
+          <p className="text-muted-foreground mt-6 max-w-2xl text-lg md:text-3xl">
+            Learn JavaScript with the help of mini-games and prepare for technical interviews! Practice algorithms,
+            problem solving, and core JS concepts in a fun way.
+          </p>
+          <div className="mt-8 flex justify-center gap-4">
+            <PrimaryButton>Sign in</PrimaryButton>
+            <PrimaryButton variant="secondary">Register</PrimaryButton>
+          </div>
+          <div className="flex items-center justify-center p-20">
+            <Image
+              src="/landing-logo-mobile.png"
+              width={1052}
+              height={1052}
+              className="block rounded-3xl md:hidden"
+              alt="JS logo"
+            />
+          </div>
+        </div>
+        <div className="p-7">
+          <Image
+            src="/landing-logo-desktop.jpg"
+            width={870}
+            height={500}
+            className="hidden rounded-3xl md:block"
+            alt="JS logo"
+          />
+        </div>
       </main>
     </div>
   );
