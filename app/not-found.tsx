ce62e7a20e26ce7c65dd2897a0120a127e2ea602
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2026-02-26 19:10:16 +0100

    feat: implement animated-counter component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link';
 
+import AnimatedCounter from '@/components/animated-counter';
 import { Button } from '@/components/ui/button';
 
 export default function NotFound() {
   return (
     <div className="mx-auto flex min-h-[calc(100vh-5rem)] w-screen flex-col items-center justify-center px-6 text-center">
       <div className="m-10">
-        <h1 className="text-5xl font-bold sm:text-6xl">404 - Not Found</h1>
+        <h1 className="text-5xl font-bold sm:text-6xl">
+          {<AnimatedCounter startValue={100} endValue={404} animationDuration={2} />}
+          <p>Not Found</p>
+        </h1>
         <p className="text-muted-foreground mt-6 max-w-2xl text-lg md:text-3xl">
           The page you are looking for does not exist.
         </p>
