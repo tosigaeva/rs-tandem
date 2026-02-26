364fdf5da2759197eb17c9fb115fc4a3d8184daa
mero93m <merabkopaleishvili1993@gmail.com>
2026-02-26 22:47:13 +0400

    fix: rename to proxy, fix matcher regex

--- proxy.ts
+++ proxy.ts
@@ -2,10 +2,10 @@ import { NextRequest } from 'next/server';
 
 import { updateSession } from './lib/supabase/middleware';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   return await updateSession(request);
 }
 
 export const config = {
-  matcher: [String.raw`/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp)$).*)`],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
