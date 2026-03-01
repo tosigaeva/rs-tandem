5db209d461b63f3af35d5368ec66f14e10444df5
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2026-03-01 21:04:18 +0100

    feat: add carousel

--- app/widget-test/page.tsx
+++ app/widget-test/page.tsx
@@ -1,17 +1,10 @@
 'use client';
 
-import { useState } from 'react';
-
 import { getMockQuestions } from '@/api/questions.mock';
 import { LearningWidget } from '@/components/widgets/learning-widget/widget';
 
 export default function Page() {
-  const [questions] = useState(() => getMockQuestions(10));
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const questions = getMockQuestions(10);
 
-  const handleNext = () => {
-    setCurrentIndex((previous) => (previous + 1) % questions.length);
-  };
-
-  return <LearningWidget question={questions[currentIndex]} onNext={handleNext} />;
+  return <LearningWidget questions={questions} />;
 }
