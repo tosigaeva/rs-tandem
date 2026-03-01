9086459d0656c2f61fde647ad195a8306366a1ed
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2026-03-01 21:04:18 +0100

    feat: add carousel

--- components/widgets/learning-widget/widget.tsx
+++ components/widgets/learning-widget/widget.tsx
@@ -1,20 +1,40 @@
 'use client';
+import { useRef, useState } from 'react';
 
+import { Button } from '@/components/ui/button';
+import type { CarouselApi } from '@/components/ui/carousel';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import { Question } from '@/types/question';
 
 import { FlipCard } from './flip-card';
 
 type LearningWidgetProperties = {
-  question: Question;
-  onNext?: () => void;
+  questions: Question[];
 };
 
-export function LearningWidget({ question, onNext }: LearningWidgetProperties) {
+export function LearningWidget({ questions }: LearningWidgetProperties) {
+  const [api, setApi] = useState<CarouselApi>();
+
   return (
     <div className="flex min-h-full flex-col items-center gap-6">
-      <FlipCard key={question.id} front={question.payload.question} back={question.payload.correctAnswer} />
-
-      {onNext && <button onClick={onNext}>Next Question</button>}
+      <Carousel
+        setApi={setApi}
+        opts={{
+          align: 'start',
+        }}
+        className="w-full"
+      >
+        <CarouselContent>
+          {questions.map((question) => (
+            <CarouselItem key={question.id} className="flex justify-center">
+              <FlipCard front={question.payload.question} back={question.payload.correctAnswer} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <Button onClick={() => api?.scrollNext()}>Next Question</Button>
     </div>
   );
 }
