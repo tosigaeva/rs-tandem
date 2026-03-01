51e7744db1a2f1648029d54715f34dd297747935
mero93m <merabkopaleishvili1993@gmail.com>
2026-03-01 23:20:35 +0400

    feat: implement password reveal button

--- app/login/custom-input.tsx
+++ app/login/custom-input.tsx
@@ -1,7 +1,9 @@
 'use client';
+import { Eye, EyeOff } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
+import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 
 const DEBOUNCE_TIMER = 300;
@@ -21,6 +23,9 @@ export const CustomInput = ({ name, label, type, placeholder, classes }: InputPr
     watch,
     trigger,
   } = useFormContext();
+
+  const isPassword = type === 'password';
+  const [visibility, visibilityToggle] = useState(false);
 
   const errorMessage = typeof errors[name]?.message === 'string' ? errors[name]?.message : '';
 
@@ -49,15 +54,28 @@ export const CustomInput = ({ name, label, type, placeholder, classes }: InputPr
   return (
     <div className={cn('flex w-full flex-col gap-1.5', classes)}>
       <label className="text-sm font-medium text-slate-700">{label}</label>
-      <input
-        {...register(name)}
-        type={type}
-        placeholder={placeholder}
-        className={cn(
-          'relative rounded-md border bg-amber-50 px-3 py-2 text-sm transition-all outline-none',
-          hasError ? 'border-red-500 bg-red-50' : 'border-slate-300 focus:border-blue-500'
+      <div className="relative flex w-fit align-middle">
+        <input
+          {...register(name)}
+          type={visibility ? 'text' : type}
+          placeholder={placeholder}
+          className={cn(
+            'rounded-md border bg-amber-50 px-3 py-2 pr-10 text-sm transition-all outline-none',
+            hasError ? 'border-red-500 bg-red-50' : 'border-slate-300 focus:border-blue-500'
+          )}
+        />
+        {isPassword && (
+          <Button
+            variant="ghost"
+            size="icon-xs"
+            onClick={() => visibilityToggle(!visibility)}
+            className="absolute top-1/2 right-1 -translate-y-1/2 text-slate-500 hover:text-slate-700"
+            tabIndex={-1}
+          >
+            {visibility ? <Eye /> : <EyeOff />}
+          </Button>
         )}
-      />
+      </div>
       <div className="min-h-5">{hasError && <p className="text-xs font-medium text-red-500">{errorMessage}</p>}</div>
     </div>
   );
