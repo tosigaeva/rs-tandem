718781bdfe6132c5b0d871e3e05873ab56ed72b3
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2026-03-07 11:59:22 +0100

    add topics request

--- api/topics.api.ts
+++ api/topics.api.ts
@@ -3,6 +3,32 @@ import { supabaseServer } from '@/lib/supabase/server';
 import { LibraryTopicsResponse, Topic } from '@/types/topic';
 
 export async function getTopicsOverview(): Promise<LibraryTopicsResponse> {
+  if (process.env.MOCK_MODE === 'production') {
+    const supabase = await supabaseServer();
+    const query = supabase.from('topics').select('id, title');
+
+    const { data, error } = await query;
+    console.log(error);
+
+    if (data === null) {
+      return {
+        userTopics: mockLibraryTopics.userTopics,
+        topics: [],
+      };
+    }
+
+    return {
+      userTopics: mockLibraryTopics.userTopics,
+      topics: data.map((topic) => ({
+        id: topic.id,
+        name: topic.title,
+        level: 'Super',
+        description: 'Mock',
+        subject: 'mock topic',
+      })),
+    };
+  }
+
   return mockLibraryTopics;
 }
 
@@ -26,5 +52,6 @@ export async function getTopic(topicId: string): Promise<Topic | undefined> {
       subject: 'mock topic',
     };
   }
+
   return mockTopics.find((topic) => topic.id === topicId);
 }
