239b2024c90273ff9c7a57a8916824f019d47034
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2026-03-10 15:13:25 +0100

    update mocks

--- api/mocks/questions.mock.ts
+++ api/mocks/questions.mock.ts
@@ -30,4 +30,94 @@ export const mockQuestions: Question[] = [
       hints: ['This method returns a new array with elements that pass the test'],
     },
   },
+  {
+    id: 'Learning-1',
+    topicId: '1',
+    type: WidgetType.Quiz,
+    payload: {
+      question: 'Closure',
+      options: ['Closure', 'undefined', 'object', 'NaN'],
+    },
+  },
+  {
+    id: 'Learning-2',
+    topicId: '1',
+    type: WidgetType.TrueFalse,
+    payload: {
+      statement: 'Hoisting',
+      explanation: '<i>Hoisting()</i> preserves input array order, regardless of completion time',
+    },
+  },
+  {
+    id: 'Learning-3',
+    topicId: '1',
+    type: WidgetType.CodeCompletion,
+    payload: {
+      code: 'Event Loop',
+      blanks: ['___'],
+      hints: ['This method returns a new array with elements that pass the test'],
+    },
+  },
+  {
+    id: 'Learning-4',
+    topicId: '1',
+    type: WidgetType.FlipCard,
+    payload: {
+      question: 'Promise',
+    },
+  },
+  {
+    id: 'Learning-5',
+    topicId: '1',
+    type: WidgetType.Quiz,
+    payload: {
+      question: 'Callback Function',
+      options: ['null', 'Callback Function', 'object', 'NaN'],
+    },
+  },
+  {
+    id: 'Learning-6',
+    topicId: '1',
+    type: WidgetType.TrueFalse,
+    payload: {
+      statement: 'Arrow Function',
+      explanation: '<i>Arrow Function()</i> preserves input array order, regardless of completion time',
+    },
+  },
+  {
+    id: 'Learning-7',
+    topicId: '1',
+    type: WidgetType.CodeCompletion,
+    payload: {
+      code: 'Prototype',
+      blanks: ['___'],
+      hints: ['This method returns a new array with elements that pass the test'],
+    },
+  },
+  {
+    id: 'Learning-8',
+    topicId: '1',
+    type: WidgetType.FlipCard,
+    payload: {
+      question: 'Destructuring',
+    },
+  },
+  {
+    id: 'Learning-9',
+    topicId: '1',
+    type: WidgetType.Quiz,
+    payload: {
+      question: 'Strict Mode',
+      options: ['null', 'Callback Function', 'Strict Mode', 'NaN'],
+    },
+  },
+  {
+    id: 'Learning-10',
+    topicId: '1',
+    type: WidgetType.TrueFalse,
+    payload: {
+      statement: 'Debouncing',
+      explanation: '<i>Debouncing()</i> preserves input array order, regardless of completion time',
+    },
+  },
 ];
--- api/trainer.api.ts
+++ api/trainer.api.ts
@@ -1,4 +1,3 @@
-import { mockLearningQuestions } from '@/api/mocks/learning-questions.mock';
 import { mockQuestions } from '@/api/mocks/questions.mock';
 import { mockLibraryTopics, mockTopics } from '@/api/mocks/topics.mock';
 import { FlipCardWidget } from '@/components/library/widget/ui/flip-card/type';
@@ -8,55 +7,55 @@ import { LibraryTopicsResponse, Topic } from '@/types/topic';
 import { WidgetType } from '@/types/widget';
 
 export async function getTopicsOverview(): Promise<LibraryTopicsResponse> {
-  if (process.env.MOCK_MODE === 'production') {
-    const supabase = await supabaseServer();
-    const query = supabase.from('topics').select('id, title');
-
-    const { data, error } = await query;
-    console.log(error);
-
-    if (data === null) {
-      return {
-        userTopics: mockLibraryTopics.userTopics,
-        topics: [],
-      };
-    }
-
-    return {
-      userTopics: mockLibraryTopics.userTopics,
-      topics: data.map((topic) => ({
-        id: topic.id,
-        name: topic.title,
-        level: 'Super',
-        description: 'Mock',
-        subject: 'mock topic',
-      })),
-    };
-  }
+  // if (process.env.MOCK_MODE === 'production') {
+  //   const supabase = await supabaseServer();
+  //   const query = supabase.from('topics').select('id, title');
+  //
+  //   const { data, error } = await query;
+  //   console.log(error);
+  //
+  //   if (data === null) {
+  //     return {
+  //       userTopics: mockLibraryTopics.userTopics,
+  //       topics: [],
+  //     };
+  //   }
+  //
+  //   return {
+  //     userTopics: mockLibraryTopics.userTopics,
+  //     topics: data.map((topic) => ({
+  //       id: topic.id,
+  //       name: topic.title,
+  //       level: 'Super',
+  //       description: 'Mock',
+  //       subject: 'mock topic',
+  //     })),
+  //   };
+  // }
 
   return mockLibraryTopics;
 }
 
 export async function getTopic(topicId: string): Promise<Topic | undefined> {
-  if (process.env.MOCK_MODE === 'production') {
-    const supabase = await supabaseServer();
-    const query = supabase.from('topics').select('id, title').eq('id', topicId).single();
-
-    const { data, error } = await query;
-    console.log(error);
-
-    if (data === null) {
-      return undefined;
-    }
-
-    return {
-      id: topicId,
-      name: data.title,
-      level: '1',
-      description: 'Mock',
-      subject: 'mock topic',
-    };
-  }
+  // if (process.env.MOCK_MODE === 'production') {
+  //   const supabase = await supabaseServer();
+  //   const query = supabase.from('topics').select('id, title').eq('id', topicId).single();
+  //
+  //   const { data, error } = await query;
+  //   console.log(error);
+  //
+  //   if (data === null) {
+  //     return undefined;
+  //   }
+  //
+  //   return {
+  //     id: topicId,
+  //     name: data.title,
+  //     level: '1',
+  //     description: 'Mock',
+  //     subject: 'mock topic',
+  //   };
+  // }
 
   return mockTopics.find((topic) => topic.id === topicId);
 }
@@ -65,41 +64,41 @@ export async function getQuestions(
   topicId: string,
   widgetType: WidgetType | undefined = undefined
 ): Promise<Question[]> {
-  if (process.env.MOCK_MODE === 'production') {
-    const supabase = await supabaseServer();
-    let query = supabase
-      .from('question_variant')
-      .select(
-        `
-    id,
-    widget_type,
-    payload,
-    question:question_id!inner (
-      topicId
-    )
-  `
-      )
-      .eq('question.topicId', topicId);
-
-    // Add widget_type filter only if defined
-    if (widgetType !== undefined) {
-      query = query.eq('widget_type', widgetType);
-    }
-
-    const { data, error } = await query;
-    console.log(error);
-
-    if (data === null) {
-      return [];
-    }
-
-    return data.map((q) => ({
-      id: q.id,
-      topicId: topicId,
-      type: q.widget_type,
-      payload: q.payload,
-    }));
-  }
+  // if (process.env.MOCK_MODE === 'production') {
+  //   const supabase = await supabaseServer();
+  //   let query = supabase
+  //     .from('question_variant')
+  //     .select(
+  //       `
+  //   id,
+  //   widget_type,
+  //   payload,
+  //   question:question_id!inner (
+  //     topicId
+  //   )
+  // `
+  //     )
+  //     .eq('question.topicId', topicId);
+  //
+  //   // Add widget_type filter only if defined
+  //   if (widgetType !== undefined) {
+  //     query = query.eq('widget_type', widgetType);
+  //   }
+  //
+  //   const { data, error } = await query;
+  //   console.log(error);
+  //
+  //   if (data === null) {
+  //     return [];
+  //   }
+  //
+  //   return data.map((q) => ({
+  //     id: q.id,
+  //     topicId: topicId,
+  //     type: q.widget_type,
+  //     payload: q.payload,
+  //   }));
+  // }
   return mockQuestions.filter((question) => question?.topicId === topicId);
 }
 
@@ -120,42 +119,7 @@ export async function validateQuestion(questionId: string, answer: string): Prom
 }
 
 export function getFlipQuestions(): (BaseQuestion & FlipCardWidget)[] {
-  return mockLearningQuestions.map((question) => {
-    let payload;
-    switch (question.type) {
-      case WidgetType.Quiz: {
-        payload = {
-          question: question.payload.question,
-        };
-        break;
-      }
-      case WidgetType.TrueFalse: {
-        payload = {
-          question: question.payload.statement,
-        };
-        break;
-      }
-      case WidgetType.CodeCompletion: {
-        payload = {
-          question: question.payload.code,
-        };
-        break;
-      }
-      case WidgetType.FlipCard: {
-        payload = {
-          question: question.payload.question,
-        };
-        break;
-      }
-    }
-
-    return {
-      id: question.id,
-      topicId: question.topicId,
-      type: WidgetType.FlipCard,
-      payload: payload,
-    };
-  });
+  return mockQuestions.filter((question) => question.type === WidgetType.FlipCard);
 }
 
 export async function getAnswer(questionId: string): Promise<string> {
