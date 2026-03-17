7ce1600910787b72b804440790f48b7d1e7bc7a7
mero93 <96851480+mero93@users.noreply.github.com>
2026-03-17 04:36:13 +0400

    create tables in supabase and start integrating database (#80)
    * feat: implement topics api calls with caching, pagination, sortable, add zod schemas for type safety, integrate with ui
    
    * fix: fix package.json conflict
    
    * fix: fix issues from PR comments
    
    * fix: fix order of recent topics
    
    * refactor: comment out tests that target previous implementation of component

---
+++ types/schemas/database-schemas.ts
@@ -0,0 +1,56 @@
+import { z } from 'zod';
+
+import { WidgetType } from '../widget';
+
+export enum Level {
+  beginner = 'beginner',
+  intermediate = 'intermediate',
+  advanced = 'advanced',
+}
+
+export enum Subject {
+  javascript = 'JavaScript',
+  typescript = 'TypeScript',
+}
+
+export const WidgetSchema = z
+  .object({
+    type: z.enum(WidgetType),
+    last_accessed_at: z.coerce.date().nullable(),
+    total_questions: z.number(),
+    correct_answers: z.number(),
+  })
+  .transform((data) => ({
+    type: data.type,
+    lastAccessedAt: data.last_accessed_at,
+    totalQuestions: data.total_questions,
+    correctAnswers: data.correct_answers,
+  }));
+
+export type Widget = z.infer<typeof WidgetSchema>;
+
+export const TopicSchema = z
+  .object({
+    id: z.number(),
+    name: z.string(),
+    level: z.enum(Level),
+    description: z.string(),
+    subject: z.enum(Subject),
+    last_accessed_at: z.coerce.date().nullable(),
+    created_at: z.coerce.date(),
+    widgets: z.array(WidgetSchema).default([]),
+  })
+  .transform((data) => ({
+    id: data.id,
+    name: data.name,
+    level: data.level,
+    description: data.description,
+    subject: data.subject,
+    lastAccessedAt: data.last_accessed_at,
+    totalQuestions: data.widgets.reduce((sum, widget) => (sum += widget.totalQuestions), 0),
+    correctAnswers: data.widgets.reduce((sum, widget) => (sum += widget.correctAnswers), 0),
+    createdAt: data.created_at,
+    widgets: data.widgets,
+  }));
+
+export type Topic = z.infer<typeof TopicSchema>;
