ae8e4f41fde91b2581a686c272efe8b7d2f19aed
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2026-03-28 17:50:35 +0100

    feat: define payload answer for questions by question-schema

--- types/question.ts
+++ types/question.ts
@@ -6,10 +6,37 @@ import {
 import { FlipCardPayload, FlipCardWidget } from '@/components/library/widget/ui/flip-card/type';
 import { QuizPayload, QuizWidget } from '@/components/library/widget/ui/quiz-widget/type';
 import { TrueFalsePayload, TrueFalseWidget } from '@/components/library/widget/ui/true-false-widget/type';
+import {
+  BigOPayloadAnswer,
+  BigOPayloadQuestion,
+  CodeCompletionPayloadAnswer,
+  CodeCompletionPayloadQuestion,
+  FlipCardPayloadQuestion,
+  QuizPayloadAnswer,
+  QuizPayloadQuestion,
+  TrueFalsePayloadAnswer,
+  TrueFalsePayloadQuestion,
+} from '@/types/schemas/question-payload-schema';
 import { WidgetType } from '@/types/widget';
 
 export type QuestionPayload = QuizPayload | TrueFalsePayload | CodeCompletionPayload | FlipCardPayload | BigOPayload;
 export type QuestionWidget = QuizWidget | TrueFalseWidget | CodeCompletionWidget | FlipCardWidget | BigOWidget;
+
+export type WidgetQuestionPayloadMap = {
+  [WidgetType.Quiz]: QuizPayloadQuestion;
+  [WidgetType.TrueFalse]: TrueFalsePayloadQuestion;
+  [WidgetType.CodeCompletion]: CodeCompletionPayloadQuestion;
+  [WidgetType.FlipCard]: FlipCardPayloadQuestion;
+  [WidgetType.BigONotation]: BigOPayloadQuestion;
+};
+
+export type WidgetAnswerPayloadMap = {
+  [WidgetType.Quiz]: QuizPayloadAnswer;
+  [WidgetType.TrueFalse]: TrueFalsePayloadAnswer;
+  [WidgetType.CodeCompletion]: CodeCompletionPayloadAnswer;
+  [WidgetType.FlipCard]: null;
+  [WidgetType.BigONotation]: BigOPayloadAnswer;
+};
 
 export type WidgetPayloadMap = {
   [WidgetType.Quiz]: QuizPayload;
@@ -18,6 +45,8 @@ export type WidgetPayloadMap = {
   [WidgetType.FlipCard]: FlipCardPayload;
   [WidgetType.BigONotation]: BigOPayload;
 };
+
+export type AnswerPayload = WidgetAnswerPayloadMap[WidgetType];
 
 export type BaseQuestion = {
   id: string;
