47f309941295b719fc29d3956af029776097ea9d
Alena Deviatova <104133016+deviatovae@users.noreply.github.com>
2026-04-13 12:17:26 -0400

    refactor: remove zustand (#160)

--- providers/global-spinner.provider.tsx
+++ providers/global-spinner.provider.tsx
@@ -3,19 +3,15 @@
 import { ReactNode } from 'react';
 
 import { SpinnerCustom } from '@/components/ui/SpinnerCustom';
-import { usePageState } from '@/store/page-state';
 
 import { useAuth } from './auth-state.provider';
 
 export function GlobalSpinnerProvider({ children }: { children: ReactNode }) {
   const { isAuthorizing } = useAuth();
-  const isPageLoading = usePageState((state) => state.isPageLoading);
-
-  const isLoading = () => isAuthorizing || isPageLoading;
 
   return (
     <>
-      {isLoading() && <SpinnerCustom />}
+      {isAuthorizing && <SpinnerCustom />}
       {children}
     </>
   );
